fbfbec5cb5b821258d03cbd4d53e3a76755af5e9
Timur4ik <tim.skobenko@mail.ru>
2025-03-10 03:15:45 +0300

    feat: init

---
+++ ConsoleApp2/dip/Case1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2.dip
+{
+    internal class Case1
+    {
+    }
+}
