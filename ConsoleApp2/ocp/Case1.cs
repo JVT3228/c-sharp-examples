546d99264a56e1b809e02542996a39f694615808
JVT3228 <153003057+JVT3228@users.noreply.github.com>
2025-04-10 06:32:54 +0300

    Update Case1.cs

--- ConsoleApp2/ocp/Case1.cs
+++ ConsoleApp2/ocp/Case1.cs
@@ -1,88 +1,82 @@
+//Здесь класс User совмещал логику хранения данных и отрисовки, что нарушает принцип OCP "закрытости для модификаций", поэтому я вынес логику отрисовки в отдельные классы-декораторы, чтобы можно было добавлять новые виды отрисовки без изменения существующего кода. + удалил не используемые в коде using ы.
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ConsoleApp2.ocp
 {
-    class Case1
+    interface IUserDecorator
     {
-        // 1. Интерфейс для стратегий отрисовки
-        public interface IDrawStrategy
+        void Decorate(User user);
+    }
+
+    class SelectedUserDecorator : IUserDecorator
+    {
+        public void Decorate(User user) 
         {
-            void Draw(User user);
+            Console.WriteLine("Drawing ellipse around user");
+        }
+    }
+
+    class ImageUserDecorator : IUserDecorator
+    {
+        public void Decorate(User user)
+        {
+            Console.WriteLine("Drawing user image");
+        }
+    }
+
+    class CoolGuyDecorator : IUserDecorator
+    {
+        public void Decorate(User user)
+        {
+            Console.WriteLine("Drawing cool glasses");
+        }
+    }
+
+    class User
+    {
+        public bool IsSelected { get; }
+        public string Image { get; }
+
+        public User(bool isSelected, string image)
+        {
+            IsSelected = isSelected;
+            Image = image;
+        }
+    }
+
+    class UserRenderer
+    {
+        private readonly List<IUserDecorator> _decorators;
+
+        public UserRenderer(IEnumerable<IUserDecorator> decorators)
+        {
+            _decorators = new List<IUserDecorator>(decorators);
         }
 
-        // 2. Базовый класс пользователя (закрыт для модификаций)
-        public class User
+        public void DrawUser(User user)
         {
-            public bool IsSelected { get; }
-            public string Image { get; }
-            private readonly List<IDrawStrategy> _drawStrategies;
-
-            public User(bool isSelected, string image, List<IDrawStrategy> strategies)
+            foreach (var decorator in _decorators)
             {
-                IsSelected = isSelected;
-                Image = image;
-                _drawStrategies = strategies; // 3. Внедрение стратегий через конструктор
-            }
-
-            public void DrawUser()
-            {
-                foreach (var strategy in _drawStrategies)
-                {
-                    strategy.Draw(this); // 4. Делегируем отрисовку стратегиям
-                }
+                decorator.Decorate(user);
             }
         }
+    }
 
-        // 5. Конкретные стратегии (открыты для расширения)
-        public class SelectionDecorator : IDrawStrategy
+    public class App
+    {
+        static void Main()
         {
-            public void Draw(User user)
+            var user = new User(isSelected: true, image: "profile.jpg");
+            var decorators = new List<IUserDecorator>
             {
-                if (user.IsSelected) DrawEllipseAroundUser(user);
-            }
+                new SelectedUserDecorator(),
+                new ImageUserDecorator(),
+                new CoolGuyDecorator()
+            };
 
-            private void DrawEllipseAroundUser(User user)
-                => Console.WriteLine("Drawing ellipse for selected user");
-        }
-
-        public class ImageRenderer : IDrawStrategy
-        {
-            public void Draw(User user)
-            {
-                if (!string.IsNullOrEmpty(user.Image))
-                    DrawImageOfUser(user);
-            }
-
-            private void DrawImageOfUser(User user)
-                => Console.WriteLine($"Rendering image: {user.Image}");
-        }
-
-        // 6. Стратегия для "крутых" пользователей (отдельный интерфейс)
-        public interface ICoolGuyDecorator : IDrawStrategy { }
-
-        public class CoolGuyGlassesDecorator : ICoolGuyDecorator
-        {
-            public void Draw(User user)
-            {
-                if (user is ICoolGuy) DrawCoolGuyGlasses(user); // Проверка интерфейса здесь
-            }
-
-            private void DrawCoolGuyGlasses(User user)
-                => Console.WriteLine("Adding cool glasses for ICoolGuy");
-        }
-
-        // 7. Пример интерфейса для "крутых" пользователей
-        public interface ICoolGuy { }
-
-        // 8. Пример использования
-        public class CoolUser : User, ICoolGuy
-        {
-            public CoolUser(bool isSelected, string image, List<IDrawStrategy> strategies)
-                : base(isSelected, image, strategies) { }
+            var renderer = new UserRenderer(decorators);
+            renderer.DrawUser(user);
         }
     }
 }
