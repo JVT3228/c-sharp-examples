3967744829637e1bc41d6dd3bd305a0c5f5f1eeb
JVT3228 <153003057+JVT3228@users.noreply.github.com>
2025-04-10 06:49:32 +0300

    Update Case2.cs

--- ConsoleApp2/isp/Case2.cs
+++ ConsoleApp2/isp/Case2.cs
@@ -1,92 +1,98 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+//Здесь интерфейс IMultiFunctionDevice нарушал принцип ISP, так как заставлял классы реализовывать методы, которые они не поддерживают (например, BasicPhone был вынужден реализовывать Browse и SendEmail с выбрасыванием исключений), поэтому я разделил его на несколько специализированных интерфейсов (ICallable, IBrowsable и др.), чтобы классы могли реализовывать только те интерфейсы, которые им действительно нужны, и убрал неиспользуемые using-директивы.
+
+using System;
 
 namespace ConsoleApp2.isp
 {
-    internal class Case2
+    public interface ICallable
     {
-        public interface IMultiFunctionDevice
+        void Call(string number);
+    }
+
+    public interface IBrowsable
+    {
+        void Browse(string url);
+    }
+
+    public interface IPhotographable
+    {
+        void TakePhoto();
+    }
+
+    public interface IEmailSender
+    {
+        void SendEmail(string recipient, string subject, string body);
+    }
+
+    public interface ISmsSender
+    {
+        void SendSms(string recipient, string message);
+    }
+
+    public interface IMusicPlayer
+    {
+        void PlayMusic();
+    }
+
+    public class SmartPhone : ICallable, IBrowsable, IPhotographable, IEmailSender, IMusicPlayer
+    {
+        public string Model { get; }
+        public string OS { get; }
+
+        public SmartPhone(string model, string os)
         {
-            void Call(string number);
-            void Browse(string url);
-            void TakePhoto();
-            void SendEmail(string recipient, string subject, string body);
+            Model = model;
+            OS = os;
         }
 
-        public class SmartPhone : IMultiFunctionDevice
+        public void Call(string number)
         {
-            public string Model { get; set; }
-            public string OS { get; set; }
-
-            public SmartPhone(string model, string os)
-            {
-                Model = model;
-                OS = os;
-            }
-
-            public void Call(string number)
-            {
-                Console.WriteLine("SmartPhone " + Model + " calling " + number);
-            }
-
-            public void Browse(string url)
-            {
-                Console.WriteLine("SmartPhone " + Model + " browsing " + url);
-            }
-
-            public void TakePhoto()
-            {
-                Console.WriteLine("SmartPhone " + Model + " takes a high quality photo");
-            }
-
-            public void SendEmail(string recipient, string subject, string body)
-            {
-                Console.WriteLine("SmartPhone " + Model + " sending email to " + recipient);
-            }
-
-            public void PlayMusic()
-            {
-                Console.WriteLine("SmartPhone " + Model + " is playing music");
-            }
+            Console.WriteLine($"SmartPhone {Model} calling {number}");
         }
 
-        public class BasicPhone : IMultiFunctionDevice
+        public void Browse(string url)
         {
-            public string Model { get; set; }
-
-            public BasicPhone(string model)
-            {
-                Model = model;
-            }
-
-            public void Call(string number)
-            {
-                Console.WriteLine("BasicPhone " + Model + " calling " + number);
-            }
-
-            public void Browse(string url)
-            {
-                throw new NotSupportedException("BasicPhone " + Model + " does not support browsing.");
-            }
-
-            public void TakePhoto()
-            {
-                Console.WriteLine("BasicPhone " + Model + " takes a very low quality photo");
-            }
-
-            public void SendEmail(string recipient, string subject, string body)
-            {
-                throw new NotSupportedException("BasicPhone " + Model + " does not support sending emails.");
-            }
-
-            public void SendSMS(string recipient, string message)
-            {
-                Console.WriteLine("BasicPhone " + Model + " sending SMS to " + recipient);
-            }
+            Console.WriteLine($"SmartPhone {Model} browsing {url}");
         }
 
+        public void TakePhoto()
+        {
+            Console.WriteLine($"SmartPhone {Model} takes a high quality photo");
+        }
+
+        public void SendEmail(string recipient, string subject, string body)
+        {
+            Console.WriteLine($"SmartPhone {Model} sending email to {recipient}");
+        }
+
+        public void PlayMusic()
+        {
+            Console.WriteLine($"SmartPhone {Model} is playing music");
+        }
+    }
+
+    public class BasicPhone : ICallable, IPhotographable, ISmsSender
+    {
+        public string Model { get; }
+
+        public BasicPhone(string model)
+        {
+            Model = model;
+        }
+
+        public void Call(string number)
+        {
+            Console.WriteLine($"BasicPhone {Model} calling {number}");
+        }
+
+        public void TakePhoto()
+        {
+            Console.WriteLine($"BasicPhone {Model} takes a very low quality photo");
+        }
+
+        public void SendSms(string recipient, string message)
+        {
+            Console.WriteLine($"BasicPhone {Model} sending SMS to {recipient}");
+        }
     }
 }
